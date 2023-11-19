940cc2aa97284fc72273041319503f6574dbc4cf
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2023-11-19 17:15:38 -0300

    Make changes to the React component
    Import the rehype-raw and remark-gfm plugins at the top of the file, remove the isWideScreen attribute from the component state and the handleResize, componentDidMount and componentWillUnmount methods.
    
    Make changes to the content within the App component's render method.

--- src/App.js
+++ src/App.js
@@ -1,89 +1,41 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
-class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        textAreaValue: "",
-        isWideScreen: window.innerWidth >= 1410, // Verifica a largura da tela no início
-      };
-    }
-  
-    handleInputChange = (event) => {
-      this.setState({ textAreaValue: event.target.value });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textAreaValue: "",
     };
-  
-    // Atualiza o estado isWideScreen quando a largura da tela muda
-    handleResize = () => {
-      this.setState({ isWideScreen: window.innerWidth >= 1410 });
-    };
-  
-    componentDidMount() {
-      window.addEventListener("resize", this.handleResize);
-    }
-  
-    componentWillUnmount() {
-      window.removeEventListener("resize", this.handleResize);
-    }
-  
-    render() {
-      return (
-        <div>
-          {this.state.isWideScreen ? (
-            // Layout para telas largas (maior ou igual a 1410px)
-            <div className=" mt-5 mb-5">
-              <div className="row">
-                <div className="col-md-6">
-                  <h1 className="text-white mb-3">Editor:</h1>
-                  <textarea
-                    style={{ border: "1px solid #1d2f2f", boxShadow: "1px 1px 10px 2px #3a5f5f" }}
-                    rows="10"
-                    cols="50"
-                    value={this.state.textAreaValue}
-                    onChange={this.handleInputChange}
-                  ></textarea>
-                </div>
-  
-                <div className="col-md-6">
-                  <h2 className="text-white">Previewer:</h2>
-                  <div
-                    id="preview"
-                    className="preview p-5 bg-white"
-                    style={{ border: "1px solid #1d2f2f", boxShadow: "1px 1px 10px 2px #3a5f5f" }}
-                  >
-                    <ReactMarkdown>{this.state.textAreaValue}</ReactMarkdown>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            // Layout para telas estreitas (menos de 1000px)
-            <div class="mt-5 mb-5"> 
-              <h1 className="text-white mb-3">Editor:</h1>
-              <textarea
-                style={{ border: "1px solid #1d2f2f", boxShadow: "1px 1px 10px 2px #3a5f5f" }}
-                rows="10"
-                cols="50"
-                value={this.state.textAreaValue}
-                onChange={this.handleInputChange}
-              ></textarea>
-              <br />
-              <br />
-              <h2 className="text-white">Previewer:</h2>
-              <div
-                id="preview"
-                className="preview p-3 bg-white"
-                style={{ border: "1px solid #1d2f2f", boxShadow: "1px 1px 10px 2px #3a5f5f" }}
-              >
-                <ReactMarkdown>{this.state.textAreaValue}</ReactMarkdown>
-              </div>
-            </div>
-          )}
-        </div>
-      );
-    }
   }
 
-export default App;
+  handleInputChange = (event) => {
+    this.setState({ textAreaValue: event.target.value });
+  };
+
+  render() {
+    return (
+      <div> 
+        <h1>Editor:</h1>
+        <textarea
+          rows="10"
+          cols="50"
+          value={this.state.textAreaValue}
+          onChange={this.handleInputChange}
+        ></textarea>
+        <br />
+        <br />
+        <br />
+        <h2>Preview:</h2>
+          <div className="preview">
+            <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>{this.state.textAreaValue}</ReactMarkdown>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App; 
