5ff71bc5c1ea3c9f2fabef01fe245f2ab1333004
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2023-11-19 17:19:38 -0300

    Undo Bootstrap imports in Javascript file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
