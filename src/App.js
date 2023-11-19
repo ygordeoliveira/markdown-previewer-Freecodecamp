08711201ac191d91c72977b87c74b117c6380fe6
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2023-11-19 17:41:53 -0300

    Fix Javascript file indentation errors

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textAreaValue: "",
+    constructor(props) {
+        super(props);
+        this.state = {
+            textAreaValue: "",
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ textAreaValue: event.target.value });
     };
-  }
 
-  handleInputChange = (event) => {
-    this.setState({ textAreaValue: event.target.value });
-  };
-
-  render() {
-    return (
-      <div> 
-        <h1>Editor:</h1>
-        <textarea
-          rows="10"
-          cols="50"
-          value={this.state.textAreaValue}
-          onChange={this.handleInputChange}
-        ></textarea>
-        <br />
-        <br />
-        <br />
-        <h2>Preview:</h2>
-          <div className="preview">
-            <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>{this.state.textAreaValue}</ReactMarkdown>
-          </div>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div> 
+                <h1>Editor:</h1>
+                <textarea
+                  rows="10"
+                  cols="50"
+                  value={this.state.textAreaValue}
+                  onChange={this.handleInputChange}
+                ></textarea>
+                <br />
+                <br />
+                <br />
+                <h2>Preview:</h2>
+                <div className="preview">
+                    <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>{this.state.textAreaValue}</ReactMarkdown>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App; 
